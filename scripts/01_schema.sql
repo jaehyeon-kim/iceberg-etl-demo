771c16c4612a8a13c7c735d99b91d8d4b1f88a6c
Jaehyeon Kim <jaehyeon.kim@macquarie.com>
2022-06-23 15:26:13 +1000

    add sample data

---
+++ scripts/01_schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE products(
+  id         bigserial PRIMARY KEY,
+  created_at timestamp,
+  category   text,
+  ean        text,
+  price      float,
+  quantity   int default(5000),
+  rating     float,
+  title      text,
+  vendor     text
+);
+
+CREATE TABLE users(
+  id         bigserial PRIMARY KEY,
+  created_at timestamp,
+  name       text,
+  email      text,
+  address    text,
+  city       text,
+  state      text,
+  zip        text,
+  birth_date text,
+  latitude   float,
+  longitude  float,
+  password   text,
+  source     text
+);
+
+CREATE TABLE orders(
+  id         bigserial PRIMARY KEY,
+  created_at timestamp,
+  user_id    bigint,
+  product_id bigint,
+  discount   float,
+  quantity   int,
+  subtotal   float,
+  tax        float,
+  total      float
+);
